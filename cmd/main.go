4d2c62d020182a21b49b51ac883e16e6557992bf
M Price <zaptross@outlook.com>
2023-08-25 01:04:50 +1000

    feat: subcommands in main

--- cmd/main.go
+++ cmd/main.go
@@ -16,6 +16,9 @@ func main() {
 		log.Fatal("No arguments provided")
 	}
 
+	action := args[0]
+	args = args[1:]
+
 	repository := args[0]
 	count := 4
 	if len(args) > 1 {
@@ -28,6 +31,17 @@ func main() {
 		count = countArg
 	}
 
+	switch action {
+	case "images":
+		imagesCount(repository, count)
+	case "latest":
+		latest(repository)
+	default:
+		log.Fatalf("Invalid action: %s", action)
+	}
+}
+
+func imagesCount(repository string, count int) {
 	images, err := godohuver.GetTagsFromRepository(repository, count)
 
 	if err != nil {
@@ -37,4 +51,14 @@ func main() {
 	for _, image := range images {
 		fmt.Printf("%s:%s - %s\n", image.Name, image.Tag, image.LastPushed)
 	}
+}
+
+func latest(repository string) {
+	latest, err := godohuver.GetLatestImage(repository)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Latest: %s:%s - %s\n", latest.Name, latest.Tag, latest.LastPushed)
 }
